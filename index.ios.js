4007797a4052ff0ff25f83a7f3aa7bada1fa3ee7
Ibrahim <buamod@gmail.com>
2017-08-08 23:52:54 -0400

    basic styling

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
+  View,
   Text,
-  View
+  AppRegistry
 } from 'react-native';
 
-export default class SimpleWeatherApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+
+class App extends Component {
+  render(){
+    return(
+  <View style={{alignContent:'center', justifyContent:'center',  flex:1, backgroundColor:'lightblue'}}>
+  <Text >Hello World</Text>
+  </View>
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('SimpleWeatherApp', () => SimpleWeatherApp);
+AppRegistry.registerComponent('SimpleWeatherApp', () => App)
