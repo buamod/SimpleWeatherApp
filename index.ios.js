bc434bbda9958d602262bda4eb1f815f52adeca3
Ibrahim <buamod@gmail.com>
2017-08-12 21:37:49 -0400

    adding the header text component with two some style components

--- index.ios.js
+++ index.ios.js
@@ -10,20 +10,47 @@ import {
 class App extends Component {
   render(){
     return(
-  <View style={generalStyles.viewStyling}>
-  <Text >Hello World</Text>
+  <View style={allStyles.container}>    
+  <View style={allStyles.headerStyle}>
+  <Text >icon</Text>  
+  <Text style={allStyles.temp}>24°</Text>  
   </View>
+
+<View style={allStyles.bodyStyle}>
+  <Text >Title</Text>
+  <Text >Sub title</Text>
+  </View>
+  </View>
+  
     )
   }
 }
 
-const generalStyles = StyleSheet.create({
-  viewStyling:{
+const allStyles = StyleSheet.create({
+container:{
+  flex:1
+},
+headerStyle:{
   alignItems:'center', 
-  justifyContent:'center',  
+  justifyContent:'space-around',  
   flex:1, 
-  backgroundColor:'lightblue'
-  }
+  backgroundColor: 'red',
+  flexDirection:'row'
+},
+temp:{
+  fontFamily:'HelveticaNeue-Bold',
+  fontSize:24,
+  color:'white'
+},
+bodyStyle:{
+  alignItems:'flex-start', 
+  justifyContent:'flex-end',  
+  flex:5, 
+  backgroundColor: 'yellow'
+
+}
 })
+
+
 
 AppRegistry.registerComponent('SimpleWeatherApp', () => App)
