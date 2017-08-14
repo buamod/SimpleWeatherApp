b59a863db0a3860eda99d96766d90e82cc1e9eed
Ibrahim <buamod@gmail.com>
2017-08-13 21:41:16 -0400

    fetching data from the internet with a simple method to update the state

--- index.ios.js
+++ index.ios.js
@@ -3,16 +3,24 @@ import {
   View,
   Text,
   AppRegistry,
-  StyleSheet
+  StyleSheet,
+  StatusBar
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import {fetchWeather} from './weatherAPI'
 
 class App extends Component {
 
+  componentWillMount(){
+    this.state = {hideStatusBar:false}
+  }
+
   componentDidMount(){
     this.getlocation()
-    fetchWeather(-21, 28)
+    fetchWeather(-21, 28).then(res => console.log(res))
+    setInterval(() => {
+      this.setState({hideStatusBar:true})
+    }, 1000)
   }
 
   getlocation(){
@@ -25,15 +33,17 @@ class App extends Component {
 
   
   render(){
+    console.log('dasdasdasd')
     return(
-  <View style={allStyles.container}>    
+  <View style={allStyles.container}>
+  <StatusBar hidden={this.state.hideStatusBar}/>    
   <View style={allStyles.headerStyle}>
   <Icon  name={'ios-sunny'} size={80} color={'white'}/>  
   <Text style={allStyles.temp}>24°</Text>  
   </View>
 
 <View style={allStyles.bodyStyle}>
-  <Text style={allStyles.title}>Title</Text>
+  <Text style={allStyles.title}>Today's <Text style={{color:'red'}}>Weather!</Text></Text>
   <Text style={allStyles.subTitle}>Sub title</Text>
   </View>
   </View>
--- weatherAPI.js
+++ weatherAPI.js
@@ -1,6 +1,13 @@
 const rootUrl = 'http://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=6660d2ece82717bbbc5388727bbb2fd6'
 
-export const fetchWeather = (lat,lon) => {
-    const url = rootUrl+'&lat='+lat+'&lon='+lon
+export const fetchWeather = (lat, lon) => {
+    const url = rootUrl + '&lat=' + lat + '&lon=' + lon
     console.log(url)
+
+return fetch(url)
+        .then(res => res.json())
+        .then(json => ({
+            temp: json.main.temp,
+            weather: json.weather[0].main
+        }))
 }
