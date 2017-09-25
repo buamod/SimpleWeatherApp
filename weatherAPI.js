86b53768f2d0f4bd9073c2f980da1f08e7f633c8
Ali Ba Wazir <alwaziry20@gmail.com>
2017-09-24 22:52:41 -0400

    Fixed the URL format and added a log to print latitude and longtude

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,6 +1,10 @@
 const rootUrl = 'http://api.openweathermap.org/data/2.5/weather?appid=6660d2ece82717bbbc5388727bbb2fd6'
 
 export const fetchWeather = (lat, lon) => {
+<<<<<<< HEAD
+=======
+	console.log("latitude= " + lat + ", Longtude= " + lon)
+>>>>>>> Fixed the URL format and added a log to print latitude and longtude
 	const url = rootUrl+'&lat='+lat+"&lon="+lon
      console.log(url)
 
