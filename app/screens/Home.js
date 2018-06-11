a1a958dabb8a58f54f2810986a7f08c1728dda8d
Ali Ba Wazir <alwaziry20@gmail.com>
2018-06-11 17:22:31 -0400

    Added weather sub details to home page UI

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -16,7 +16,7 @@ import Highlight from 'react-native-highlight-words'
 
 const defaultState = {
   cityName: 'Ottawa',
-  lastUpdated: 'date + time',
+  lastUpdatedTime: 'date + time',
   currentWeather: {
     weatherCond: 'Default',
     temp: '20',
