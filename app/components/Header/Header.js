3c293ee4b95d09dd750efc63fd5428362a0b61bb
Ali Ba Wazir <alwaziry20@gmail.com>
2018-06-17 00:19:39 -0400

    Moved DeveloperInfo from Home class to Header class

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,33 +1,79 @@
-import React, {PropTypes} from 'react';
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+import { DeveloperInfo } from '../DeveloperInfo';
 import styles from './styles';
 
-const Header= ({ cityName, date, onMenuButtonPress, onInfoButtonPress })=> (
-  <View style= {styles.container}>
-    <TouchableOpacity style= {styles.leftButton} onPress= {onMenuButtonPress}>
-      <Icon name={'md-menu'}  style= {styles.icon}> </Icon>
-    </TouchableOpacity>
-    <View style= {styles.headline}>
-      <Text style= {styles.cityName}>
-        {cityName}
-      </Text>
-      <Text style= {styles.date}>
-        {date}
-      </Text>
-    </View>
-    <TouchableOpacity style= {styles.rightButton} onPress= {onInfoButtonPress}>
-      <Icon name={'md-information-circle'}  style= {styles.icon}> </Icon>
-    </TouchableOpacity>
-  </View>
-);
-
-Header.PropTypes={
+class Header extends Component{
+  static propTypes = {
     cityName: PropTypes.string,
     date: PropTypes.string,
-    onMenuButtonPress: PropTypes.func,
-    onInfoButtonPress: PropTypes.func,
-};
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isDevInfoVisible: false,
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  componentWillUnmount() {
+  }
+
+  handleMenuButtonPress= ()=> {
+    console.log('Menu button pressed');
+  };
+
+  handleInfoButtonPress= ()=> {
+    console.log('Info button pressed');
+    this.showDevInfo();
+  };
+
+  setDevInfoVisibility= (visible)=>{
+    this.setState({
+      isDevInfoVisible: visible,
+    });
+  };
+
+  showDevInfo= ()=>{
+    console.log('Will show developer info');
+    this.setDevInfoVisibility(true);
+  };
+
+  handleExitDevInfo= ()=>{
+    console.log('Will hide developer info');
+    this.setDevInfoVisibility(false);
+  };
+  render(){
+    return(
+      <View style= {styles.container}>
+        <TouchableOpacity style= {styles.leftButton} onPress= {this.handleMenuButtonPress}>
+          <Icon name={'md-menu'}  style= {styles.icon}> </Icon>
+        </TouchableOpacity>
+        <View style= {styles.headline}>
+          <Text style= {styles.cityName}>
+            {this.props.cityName}
+          </Text>
+          <Text style= {styles.date}>
+          {this.props.date}
+          </Text>
+        </View>
+        <TouchableOpacity style= {styles.rightButton} onPress= {this.handleInfoButtonPress}>
+          <Icon name={'md-information-circle'}  style= {styles.icon}> </Icon>
+        </TouchableOpacity>
+        <DeveloperInfo
+          visible= {this.state.isDevInfoVisible}
+          onExitButtonPress= {this.handleExitDevInfo}
+        />
+      </View>
+    );
+  }
+}
 
 export default Header;
