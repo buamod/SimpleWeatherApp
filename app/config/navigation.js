9f0d5873e3dd870b5bcaf6f2c13138744344dab8
Ali Ba Wazir <alwaziry20@gmail.com>
2018-06-17 01:23:44 -0400

    Added icons to TabNavigator

--- app/config/navigation.js
+++ app/config/navigation.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import {createBottomTabNavigator } from 'react-navigation';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Home from '../screens/Home';
 import HourlyForcast from '../screens/HourlyForcast';
 
-export const Tab = createBottomTabNavigator ({
-    Home: {screen: Home},
-    HourlyForcast: {screen: HourlyForcast},
-});
+export const Tab = createBottomTabNavigator (
+    {
+        Home: {screen: Home},
+        HourlyForcast: {screen: HourlyForcast},
+    },
+    {
+        navigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({ focused, tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName;
+                if (routeName === 'Home') {
+                    iconName = 'md-home';
+                } else if (routeName === 'HourlyForcast') {
+                    iconName = 'md-time';
+                }
+                return <Ionicons name={iconName} size={30} color={tintColor} />;
+            },
+        }),
+        tabBarOptions: {
+            showLabel: false,
+            style: {
+                backgroundColor: 'white',
+            },
+            activeTintColor: 'blue',
+            inactiveTintColor: 'gray',
+        },
+    }
+);
