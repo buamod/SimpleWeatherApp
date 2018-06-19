b24e9c965c722abc3e4978166eb98561ae422e8f
Ali Ba Wazir <alwaziry20@gmail.com>
2018-06-19 00:46:17 -0400

    Changed times to AM and PM in hourly forcast screen

--- app/data/weatherData.js
+++ app/data/weatherData.js
@@ -1,32 +1,37 @@
 export default defaultHourlyForcast= [
     {
-        hour: '10',
+        hour: '10 AM',
         weatherCond: 'Default',
         temp: '20',
     },
     {
-        hour: '11',
+        hour: '11 AM',
         weatherCond: 'Clear',
         temp: '20',
     },
     {
-        hour: '12',
+        hour: '12 PM',
         weatherCond: 'Rain',
         temp: '20',
     },
     {
-        hour: '13',
+        hour: '1 PM',
         weatherCond: 'Thunderstorm',
-        temp: '20',
+        temp: '2 PM',
     },
     {
-        hour: '14',
+        hour: '3 PM',
         weatherCond: 'Clouds',
         temp: '20',
     },
     {
-        hour: '15',
+        hour: '4 PM',
         weatherCond: 'Snow',
         temp: '20',
+    },
+    {
+        hour: '',
+        weatherCond: '',
+        temp: '',
     }
 ]
