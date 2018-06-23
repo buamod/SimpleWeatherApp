a3504fa55b67217a1f1711610b0f5d06ac7418c8
Ali Ba Wazir <alwaziry20@gmail.com>
2018-06-23 14:17:02 -0400

    Integrated app with remote API to fetch current weather and forecast data

--- app/actions/forcast.js
+++ app/actions/forcast.js
@@ -1,5 +1,8 @@
 export const UPDATE_FORCAST_DATA = 'UPDATE_FORCAST_DATA';
 
+export const FORCAST_DATA_RESULT = 'FORCAST_DATA_RESULT';
+export const FORCAST_DATA_ERROR = 'FORCAST_DATA_ERROR';
+
 export const updateForcast = () => ({
     type: UPDATE_FORCAST_DATA,
 });
