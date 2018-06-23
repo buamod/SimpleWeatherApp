a28ee57742328e4a958ac175efa61f37fa54822c
Ali Ba Wazir <alwaziry20@gmail.com>
2018-06-23 00:40:13 -0400

    setup redux-saga middleware

--- app/config/store.js
+++ app/config/store.js
@@ -1,13 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import createSagaMiddleware from 'redux-saga'; //a function
 
 import reducers from '../reducers';
+import rootSaga from './sagas';
+sagaMiddleware= createSagaMiddleware();
 
-const middleware = [];
+const middleware = [sagaMiddleware];
 if (process.env.NODE_ENV === 'development') {
-  middleware.push(logger);
+  middleware.push(logger); //logger should be the last middleware pushed to middleware array so it listens to all events
 }
 
 const store = createStore(reducers, applyMiddleware(...middleware));
 
+sagaMiddleware.run(rootSaga);
 export default store;
