e7a40298a9291a0c4c536d309166177e7d5f80f7
Ali Ba Wazir <alwaziry20@gmail.com>
2018-06-24 22:32:41 -0400

    added files skipped during git rebase

---
+++ app/components/Forcast/Forcast.js
@@ -0,0 +1,57 @@
+import React, {PropTypes} from 'react';
+import { View } from 'react-native';
+import {DayWeatherSummary} from '../DayWeatherSummary';
+
+import styles from './styles';
+
+const Forcast= ({tomorrow, afterTomorrow, afterAfterTomorrow})=> (
+  <View style= {styles.container}>
+    <DayWeatherSummary
+        day= {tomorrow['day']}
+        weatherCond= {tomorrow['weatherCond']}
+        minTemp= {tomorrow['minTemp']}
+        maxTemp= {tomorrow['maxTemp']}
+    />
+    <DayWeatherSummary
+        day= {afterTomorrow['day']}
+        weatherCond= {afterTomorrow['weatherCond']}
+        minTemp= {afterTomorrow['minTemp']}
+        maxTemp= {afterTomorrow['maxTemp']}
+    />
+    <DayWeatherSummary
+        day= {afterAfterTomorrow['day']}
+        weatherCond= {afterAfterTomorrow['weatherCond']}
+        minTemp= {afterAfterTomorrow['minTemp']}
+        maxTemp= {afterAfterTomorrow['maxTemp']}
+    />
+  </View>
+);
+
+Forcast.PropTypes= {
+    tomorrow: PropTypes.any,
+    afterTomorrow: PropTypes.any,
+    afterAfterTomorrow: PropTypes.any,
+};
+export default Forcast;
+
+/*
+
+
+        day= {tomorrow['day']}
+        weatherCond= {tomorrow['weatherCond']}
+        minTemp= {tomorrow['minTemp']}
+        maxTemp= {tomorrow['maxTemp']}
+
+    <DayWeatherSummary
+        day= {forcastSummary[1].day}
+        weatherCond= {forcastSummary[1][weatherCond]}
+        minTemp= {forcastSummary[1][minTemp]}
+        maxTemp= {forcastSummary[1][maxTemp]}
+    />
+    <DayWeatherSummary
+        day= {forcastSummary[2].day}
+        weatherCond= {forcastSummary[2][weatherCond]}
+        minTemp= {forcastSummary[2][minTemp]}
+        maxTemp= {forcastSummary[2][maxTemp]}
+    />
+*/
---
+++ app/components/Forcast/styles.js
@@ -0,0 +1,12 @@
+import EstyleSheet from 'react-native-extended-stylesheet';
+
+const styles= EstyleSheet.create({
+  container:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+});
+
+export default styles;
