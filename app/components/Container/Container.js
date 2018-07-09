46b2d484b6f563899db94e33dd820b018f5fbc95
Ali Ba Wazir <alwaziry20@gmail.com>
2018-07-09 18:02:01 -0400

    Replaced Image component with ImageBackground

--- app/components/Container/Container.js
+++ app/components/Container/Container.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { View, Image } from 'react-native';
+import { View, ImageBackground } from 'react-native';
 
 import styles from './styles';
 
 const Container= ({ children })=>(
     <View style= {styles.container}>
-      <Image 
+      <ImageBackground 
         source= {require('./images/background.jpg')}
         resizeMode= 'cover'
         style={styles.backgroundImage}
       >
         {children}
-      </Image>
+      </ImageBackground>
     </View>
 );
 
